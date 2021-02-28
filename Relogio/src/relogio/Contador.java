10e3f01d2316b1a69dfdeb7f5ccd8a6ed097e9b0
jh-soares <78885065+jh-soares@users.noreply.github.com>
2021-02-27 22:00:18 -0300

    Add files via upload

---
+++ Relogio/src/relogio/Contador.java
@@ -0,0 +1,27 @@
+package relogio;
+
+public class Contador {
+    
+    Ponteiro ponteiroHora;
+    Ponteiro ponteiroMinuto;
+    Ponteiro ponteiroSegundo;
+    
+    public void acertarRelogio(int hora, int minuto, int segundo){
+        
+        
+    }
+    
+    public int lerHora(){
+        
+    }
+    
+    public int lerMinuto(){
+        
+    }
+    
+    public int lerSegundo(){
+        
+    }
+    
+    
+}
